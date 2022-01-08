528e971b247785b005ad1e1380c73c419c638742
Dev Sivakumar <dev.sivakumar.dev@gmail.com>
2022-01-08 23:24:29 +0400

    Added language feature

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -7,8 +7,9 @@ const Movie = (props) => {
                 <div class='img-container'>
                   <img src={'https://image.tmdb.org/t/p/original/'+ props.value.poster_path} alt="No Image Avaialable" width="250px" height="240px"></img>
                 </div>
-                <h5>{props.value.title}</h5>
-                <h5>{props.value.vote_average}</h5>
+                <label style={{"fontSize" : "20px"}}>{props.value.title}</label><br></br>
+                <label style={{"fontSize" : "20px"}}>{props.value.vote_average}</label>
+                <label style={{"marginLeft" : "180px" , "fontSize" : "17px"}}>{props.value.original_language}</label>
             </td>
         </>
      );
